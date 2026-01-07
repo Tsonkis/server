5562cedc4a9432b6d759b4743e922b827f54bfc4
Tsonkis <tsouki1123@gmail.com>
2026-01-07 10:27:40 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y openjdk-21-jdk
+sudo apt install -y curl
+curl -OJ https://meta.fabricmc.net/v2/versions/loader/1.21.11/0.18.4/1.1.1/server/jar
+curl -O https://tsonkis.github.io/server/mods.zip \
+     -O https://tsonkis.github.io/server/start.sh \
+     -O https://tsonkis.github.io/server/server.properties
+unzip mods.zip
+rm -r mods.zip
+chmod +x start.sh
+java -Xmx8G -Xms2G -jar fabric-server-mc.1.21.11-loader.0.18.4-launcher.1.1.1.jar nogui
+echo start your server with bash start.sh
+
